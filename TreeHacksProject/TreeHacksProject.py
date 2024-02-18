c0aaa6fa89578b4c900378800348e2c69f04e98f
maxdittgen <myd4@cornell.edu>
2024-02-18 05:43:35 -0800

    Implemented loading screens and back buttons

--- TreeHacksProject/TreeHacksProject.py
+++ TreeHacksProject/TreeHacksProject.py
@@ -7,6 +7,7 @@ import json
 import time
 import sys
 import os
+import asyncio
 
 sys.path.append(os.getcwd())
 
@@ -40,6 +41,8 @@ class searchbarState(rx.State):
     result_summary1: str = ""
     result_summary2: str = ""
     result_summary3: str = ""
+    loading: bool = True
+    loading_text: str = "Initializing search..."
 
     """The states of the question pane"""
     prompt_question: str = ""
@@ -69,32 +72,64 @@ class searchbarState(rx.State):
                     time.sleep(0.1)
     
     #functions to run a search
-    def get_results(self):
+    async def get_results(self):
+        self.loading = True
+        yield # allow for other thread to launch loading screen
+        self.loading_text = "Searching the web..."
         self._results = srcr.search(self.text)
         self.result_title1 = self._results.results[0][0]
         self.result_link1 = self._results.results[0][1]
-        self.result_summary1 = summarizer.get_summary(self.result_link1)
         self.result_title2 = self._results.results[1][0]
         self.result_link2 = self._results.results[1][1]
-        self.result_summary2 = summarizer.get_summary(self.result_link2)
         self.result_title3 = self._results.results[2][0]
         self.result_link3 = self._results.results[2][1]
+        self.loading_text = "Summarizing results..."
+        self.result_summary1 = summarizer.get_summary(self.result_link1)
+        self.result_summary2 = summarizer.get_summary(self.result_link2)
         self.result_summary3 = summarizer.get_summary(self.result_link3)
 
         # generate questions
+        self.loading_text = "Generating prompts..."
         self.prompt_question = qg.generate_follow_up_question(self.text, self._previous_questions)
         self.choices, self.prompt_question = qg.get_multiple_choice(self.prompt_question)
         self._previous_questions.append(self.prompt_question)
         self.num_choices = len(self.choices)
+        self.loading = False
 
     #function to answer a prompt
     def answer_prompt_mc(self, choice: int):
         self.prompt_answer = self.choices[choice]
         self.text = qg.refine_query(self.text, self.prompt_answer, self.prompt_question)
+        self.loading = True
+        return searchbarState.get_results()
 
     def answer_prompt_open(self):
         self.prompt_answer = self.other_box
         self.text = qg.refine_query(self.text, self.prompt_answer, self.prompt_question)
+        self.loading = True
+        return searchbarState.get_results()
+    
+    def reset_values(self):
+        self._results = None
+        self.result_link1 = ""
+        self.result_title1 = ""
+        self.result_link2 = ""
+        self.result_title2 = ""
+        self.result_link3 = ""
+        self.result_title3 = ""
+        self.result_summary1 = ""
+        self.result_summary2 = ""
+        self.result_summary3 = ""
+        self.loading = True
+        self.loading_text = "Initializing search..."
+        self.prompt_question = ""
+        self._previous_questions = []
+        self.choices = []
+        self.num_choices = 0
+        self.other_box = ""
+        self.selected_answer = 0
+        self.prompt_answer = ""
+
     
 
 
@@ -145,11 +180,27 @@ def index() -> rx.Component:
 @rx.page(on_load=searchbarState.get_results)
 def search() -> rx.Component:
     return rx.box( 
-        rx.center(
-            rx.vstack(
-                rx.heading("Search Results:", size="7"),
-                padding="10px"
-            )
+        rx.hstack(
+            rx.link(
+                rx.card("New Search",
+                        color="white",
+                        background_color="blue",
+                        _hover={
+                            "color": "blue",
+                            "background_color": "white",
+                            "cursor": "pointer"
+                        },
+                        size="2",
+                        on_click=searchbarState.reset_states),
+                href="/"
+            ),
+            # rx.center(
+            #     rx.vstack(
+            #         rx.heading("Search Results:", size="7"),
+            #         padding="10px"
+            #     )
+            # ),
+            padding="10px"
         ),
         rx.box(
             rx.hstack(
@@ -162,92 +213,104 @@ def search() -> rx.Component:
             align = "start",
             padding = "5px"
         ),
-        rx.grid(
-            rx.card(rx.heading("Improve your search:", size="7"),
-                    rx.box(
-                        rx.heading(
-                            searchbarState.prompt_question,
-                            color = "navy"
-                        ),
-                        rx.flex(
-                            rx.foreach(
-                                rx.Var.range(searchbarState.num_choices - 1),
-                                lambda i: rx.card(searchbarState.choices[i],
-                                                  on_click=searchbarState.answer_prompt_mc(i),
-                                                  _hover={
-                                                    "color": "white",
-                                                    "background_color": "navy",
-                                                    "cursor": "pointer"})
+        # show loading screen if results are not ready yet
+        rx.cond(
+            searchbarState.loading,
+            rx.center(
+                rx.vstack(
+                    rx.chakra.circular_progress(is_indeterminate=True),
+                    rx.text(searchbarState.loading_text,color="navy"),
+                    align="center"
+                ),
+                height="75vh"
+            ),
+            rx.grid(
+                rx.card(rx.heading("Improve your search:", size="7"),
+                        rx.box(
+                            rx.heading(
+                                searchbarState.prompt_question,
+                                color = "navy"
                             ),
-                            rx.card(
-                                rx.hstack(
-                                    rx.text("Other:"),
-                                    rx.input(on_change=searchbarState.set_other_box),
-                                    rx.button(
-                                        "Go",
-                                        size="2",
-                                        on_click=searchbarState.answer_prompt_open
+                            rx.flex(
+                                rx.foreach(
+                                    rx.Var.range(searchbarState.num_choices - 1),
+                                    lambda i: rx.card(searchbarState.choices[i],
+                                                    on_click=searchbarState.answer_prompt_mc(i),
+                                                    _hover={
+                                                        "color": "white",
+                                                        "background_color": "navy",
+                                                        "cursor": "pointer"})
+                                ),
+                                rx.card(
+                                    rx.hstack(
+                                        rx.text("Other:"),
+                                        rx.input(on_change=searchbarState.set_other_box),
+                                        rx.button(
+                                            "Go",
+                                            size="2",
+                                            on_click=searchbarState.answer_prompt_open
+                                        )
                                     )
-                                )
+                                ),
+                                spacing = "4",
+                                direction="column",
+                            )
+                        ),
+                        width="100%",
+                        background_color="var(--plum-2)",
+                        border_radius="15px",
+                        padding = "5px",
+                ),
+                rx.card(
+                    rx.heading("Results", size="7"),
+                    rx.box(
+                        rx.flex(
+                            rx.link(
+                                rx.card(
+                                    rx.heading(searchbarState.result_title1),
+                                    rx.text(searchbarState.result_summary1),
+                                    _hover={
+                                        "color": "white",
+                                        "background_color": "navy",
+                                        "cursor": "pointer"}
+                                ),
+                                href=searchbarState.result_link1
+                            ),
+                            rx.divider(),
+                            rx.link(
+                                rx.card(
+                                    rx.heading(searchbarState.result_title2),
+                                    rx.text(searchbarState.result_summary2),
+                                    _hover={
+                                        "color": "white",
+                                        "background_color": "navy",
+                                        "cursor": "pointer"}
+                                ),
+                                href=searchbarState.result_link2
+                            ),
+                            rx.divider(),
+                            rx.link(
+                                rx.card(
+                                    rx.heading(searchbarState.result_title3),
+                                    rx.text(searchbarState.result_summary3),
+                                    _hover={
+                                        "color": "white",
+                                        "background_color": "navy",
+                                        "cursor": "pointer"}
+                                ),
+                                href=searchbarState.result_link3
                             ),
                             spacing = "4",
                             direction="column",
-                        )
-                    ),
-                    width="100%",
-                    background_color="var(--plum-2)",
-                    border_radius="15px",
-                    padding = "5px",
-            ),
-            rx.card(
-                rx.heading("Results", size="7"),
-                rx.box(
-                    rx.flex(
-                        rx.link(
-                            rx.card(
-                                rx.heading(searchbarState.result_title1),
-                                rx.text(searchbarState.result_summary1),
-                                _hover={
-                                    "color": "white",
-                                    "background_color": "navy",
-                                    "cursor": "pointer"}
-                            ),
-                            href=searchbarState.result_link1
                         ),
-                        rx.divider(),
-                        rx.link(
-                            rx.card(
-                                rx.heading(searchbarState.result_title2),
-                                rx.text(searchbarState.result_summary2),
-                                _hover={
-                                    "color": "white",
-                                    "background_color": "navy",
-                                    "cursor": "pointer"}
-                            ),
-                            href=searchbarState.result_link2
-                        ),
-                        rx.divider(),
-                        rx.link(
-                            rx.card(
-                                rx.heading(searchbarState.result_title3),
-                                rx.text(searchbarState.result_summary3),
-                                _hover={
-                                    "color": "white",
-                                    "background_color": "navy",
-                                    "cursor": "pointer"}
-                            ),
-                            href=searchbarState.result_link3
-                        ),
-                        spacing = "4",
-                        direction="column",
-                    ),
-                    width="100%",
-                    background_color="var(--plum-2)",
-                    border_radius="15px",
-                    padding = "5px",
-                )
-            ),
-            columns = "2"
+                        width="100%",
+                        background_color="var(--plum-2)",
+                        border_radius="15px",
+                        padding = "5px",
+                    )
+                ),
+                columns = "2"
+            )
         )
     )
 
